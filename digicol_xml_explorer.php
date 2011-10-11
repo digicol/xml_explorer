bb55d5e7baf3466239f155d6836d7009527bb5a0
Tim Strehle <tim@digicol.de>
2011-10-11 16:39:52 +0200

    Switched from the ancient xml_parse_into_struct() to DOM parsing. This makes the --encoding argument obsolete, it has been removed. Added support for XML namespaces: All namespaces are listed, and tags are displayed with a namespace prefix. You can define a prefix using the --xmlns argument if the document element does not define one and you don't like the auto-generated "ns1" prefix. The --select argument now uses true XPath selectors. ToDo: Detection of mixed content and empty tags currently not implemented.

--- digicol_xml_explorer.php
+++ digicol_xml_explorer.php
@@ -2,9 +2,7 @@
 
 // Licensed under the PHP License
 //
-// This is old code, not well-documented, and it shouldn't use 
-// xml_parse_into_struct() :-)
-//
+// This is experimental code, not well-documented...
 // Use at your own risk!
 //
 // Tim Strehle <tim@digicol.de>
@@ -16,6 +14,7 @@ class Digicol_Xml_Explorer
 {
     protected $select = array();
     protected $stats = false;
+    protected $namespace_prefixes = array();
     protected $exit_on_unknown_options = true;
     
     
@@ -36,13 +35,14 @@ class Digicol_Xml_Explorer
 
   Usage: php digicol_xml_explorer.php [ OPTIONS ] <file> [<file> ...]
 
-    -e, --encoding <encoding>  Encoding of the input XML files:
-                           "ISO-8859-1" or "UTF-8". (Default: "UTF-8")
+    -s, --select <tag>          Select values of these XPath expressions and list 
+                                them with counts.
+                                (Optional, multiple values allowed).
 
-    -s, --select <tag>     Select values of these tags or pseudo-XPath
-                           expressions and list them with counts.
-                           (Optional, multiple values allowed).
-
+        --xmlns prefix=uri      Define namespace prefixes to be used in output.
+                                If you don't set this option, namespaces will be
+                                displayed using auto-generated prefixes, e.g. ns1.
+        
     -h, --help             Display this help message and exit.
 
   Copyright 2003-2011 by Digital Collections Verlagsgesellschaft mbH.
@@ -80,7 +80,7 @@ EOT;
     {
         return array
         (
-        	array( 'encoding, e' ,  1  ),
+            array( 'xmlns'       , '+' ),
         	array( 'select, s'   , '+' ),
             array( 'help, h'     ,  0  )
         );
@@ -133,9 +133,26 @@ EOT;
         	'all_tags'        => array(),
         	'all_attributes'  => array(),
         	'all_xpaths'      => array(),
+        	'all_namespaces'  => array(),
         	'select' => array()
     	);
 
+        $this->namespace_prefixes = array();
+        
+        if (isset($this->options[ 'xmlns' ]))
+        {
+            foreach ($this->options[ 'xmlns' ] as $str)
+            {
+                if (! preg_match('/^([a-z0-9]+)=(.+)$/i', $str, $matches))
+                {
+                    fwrite(STDERR, "Wrong --xmlns parameter format.\n");
+                    exit(1);
+                }
+                    
+                $this->namespace_prefixes[ $matches[ 2 ] ] = $matches[ 1 ];
+            }
+        }
+        
         $this->select = array();
 
         if (isset($this->options[ 'select' ]))
@@ -144,17 +161,6 @@ EOT;
 
             foreach ($this->select as $sel)
                 $this->stats[ 'select' ][ $sel ] = array();
-        }
-
-        $encodings = array( 'ISO-8859-1', 'UTF-8' );
-
-        if (! isset($this->options[ 'encoding' ]))
-            $this->options[ 'encoding' ] = 'UTF-8';
-
-        if (! in_array($this->options[ 'encoding' ], $encodings))
-        {
-            fwrite(STDOUT, sprintf("Wrong encoding '%s'. Must be '%s'.\n", $this->options[ 'encoding' ], implode("' or '", $encodings)));
-            $this->cleanupAndExit(1);
         }
 
         // Read from STDIN?
@@ -213,6 +219,10 @@ EOT;
                 $this->processFile($filename, 'filename');
         }
 
+        // Clean up namespaces
+        
+        $this->cleanupNamespaces();
+
         // Summarize
 
         $this->summarize();
@@ -259,6 +269,11 @@ EOT;
             array_merge($this->stats[ 'all_attributes' ], $results[ 'all_attributes' ])
         );
 
+        $this->stats[ 'all_namespaces' ] = array_unique
+        (
+            array_merge($this->stats[ 'all_namespaces' ], $results[ 'all_namespaces' ])
+        );
+
         foreach ($results[ 'select' ] as $key => $values)
         {
             foreach ($values as $value)
@@ -297,24 +312,6 @@ EOT;
     }
 
 
-    protected function mkTagInfo($key, $value, &$results)
-    {
-        if (! isset($results[ 'all_xpaths' ][ $key ]))
-            $results[ 'all_xpaths' ][ $key ] = array( 'm' => 0, 'l' => 0, 'b' => false );
-
-        $results[ 'all_xpaths' ][ $key ][ 'm' ]++;
-
-        $value = trim($value);
-        $l = strlen($value);
-
-        if ($l > $results[ 'all_xpaths' ][ $key ][ 'l' ])
-            $results[ 'all_xpaths' ][ $key ][ 'l' ] = $l;
-
-        if (is_int(strpos($value, "\n")) || is_int(strpos($value, "\r")))
-            $results[ 'all_xpaths' ][ $key ][ 'b' ] = true;
-    }
-
-
     protected function checkXML($buffer, &$results, $filename)
     {
         $results = array
@@ -322,147 +319,209 @@ EOT;
             'all_tags'       => array(),
             'all_attributes' => array(),
             'all_xpaths'     => array(),
+            'all_namespaces' => array(),
             'select' => array()
         );
 
         // Parse XML file
 
-        $parser = xml_parser_create($this->options[ 'encoding' ]);
+        $dom = new DOMDocument();
 
-        xml_parser_set_option($parser, XML_OPTION_CASE_FOLDING, 0);
-        xml_parser_set_option($parser, XML_OPTION_SKIP_WHITE, 0);
+        $ok = $dom->loadXML($buffer);
 
-        $ok = xml_parse_into_struct($parser, $buffer, $tags, $index);
-
-        if (! $ok)
+        if ($ok === false)
         {
-            $code = xml_get_error_code($parser);
-            fwrite(STDERR, sprintf("\nCould not parse XML in <%s> (%s): %s on line %s\n", $filename, $code, xml_error_string($code), xml_get_current_line_number($parser)));
-            xml_parser_free($parser);
+            // XXX we should output error information from the XML parser!
+            fwrite(STDERR, sprintf("\nCould not parse XML in <%s>\n", $filename));
             return -1;
         }
 
-        xml_parser_free($parser);
-
-        $levels = array();
-
-        foreach ($tags as $tag)
+        // Look for namespaces registered on the root tag
+        // XXX should we do this lookup for every tag?
+        
+        $xpath = new DOMXPath($dom);
+        
+        foreach ($xpath->query('namespace::*', $dom->documentElement) as $xmlns_node) 
         {
-            // Build "XPath" string (which you can use to find out where in the hierarchy you currently are)
-
-            $levels[ $tag[ 'level' ] ] = $tag[ 'tag' ];
-
-            $xpath = '';
-
-            for ($i = 1; $i <= $tag[ 'level' ]; $i++)
-                $xpath .= '/' . $levels[ $i ];
-
-            $results[ 'all_tags' ][ ] = $tag[ 'tag' ];
-
-            // Check selected tag values
-
-            $do_select = false;
-
-            if (in_array($xpath, $this->select) || in_array($tag[ 'tag' ], $this->select))
-                $do_select = true;
-
-            // Build attribute string
-
-            if (isset($tag[ 'attributes' ]))
-            {
-                foreach ($tag[ 'attributes' ] as $name => $value)
-                {
-                    $results[ 'all_attributes' ][ ] = $name;
-
-                    if (trim($value) == '')
-                    {
-                        $key = $xpath . '@' . $name . ' (empty)';
-                    }
-                    else
-                    {
-                        $key = $xpath . '@' . $name;
-
-                        if (in_array($key, $this->select))
-                        {
-                            if (! isset($results[ 'select' ][ $key ]))
-                                $results[ 'select' ][ $key ] = array();
-                                
-                            $results[ 'select' ][ $key ][ ] = $value;
-                        }
-                    }
-
-                    $this->mkTagInfo($key, $value, $results);
-                }
-            }
-
-            // Opening tag
-
-            if ($tag[ 'type' ] == 'open')
-            {
-                if (! isset($tag[ 'value' ]))
-                    $tag[ 'value' ] = '';
-
-                if (trim($tag[ 'value' ]) == '')
-                {
-                    $key = $xpath . ' (open)';
-                    $tag[ 'value' ] = '';
-                }
-                else
-                {
-                    $key = $xpath . ' (open with value)';
-                }
-
-                $this->mkTagInfo($key, $tag[ 'value' ], $results);
-            }
-
-            // Empty or complete tag
-
-            elseif ($tag[ 'type' ] == 'complete')
-            {
-                if (! isset($tag[ 'value' ]))
-                    $tag[ 'value' ] = '';
-
-                if (trim($tag[ 'value' ]) == '')
-                {
-                    $key = $xpath . ' (empty)';
-                    $tag[ 'value' ] = '';
-                }
-                else
-                {
-                    $key = $xpath;
-
-                    if ($do_select)
-                    {
-                        if (! isset($results[ 'select' ][ $key ]))
-                            $results[ 'select' ][ $key ] = array();
-                            
-                        $results[ 'select' ][ $key ][ ] = trim($tag[ 'value' ]);
-                    }
-                }
-
-                $this->mkTagInfo($key, $tag[ 'value' ], $results);
-            }
-
-            // CDATA tag (mixed or very long content, and whitespace between tags)
-
-            elseif (($tag[ 'type' ] == 'cdata') && isset($tag[ 'value' ]))
-            {
-                if (trim($tag[ 'value' ]) != '')
-                {
-                    $key = $xpath . ' (cdata)';
-
-                    $this->mkTagInfo($key, $tag[ 'value' ], $results);
-                }
-            }
+            // Skip built-in xmlns:xml="http://www.w3.org/XML/1998/namespace"
+            
+            if ($xmlns_node->prefix === 'xml')
+                continue;
+                
+            if (! in_array($xmlns_node->nodeValue, $results[ 'all_namespaces' ]))
+                $results[ 'all_namespaces' ][ ] = $xmlns_node->nodeValue;
+        
+            if (strlen($xmlns_node->prefix) > 0)
+                $this->namespace_prefixes[ $xmlns_node->nodeValue ] = $xmlns_node->prefix;
         }
 
-        foreach ($results[ 'select' ] as $key => $values)
-            $results[ 'select' ][ $key ] = array_unique($results[ 'select' ][ $key ]);
+        // Start recursive processing on root node
 
-        return 1;
+        $this->processNode($dom->documentElement, '/', &$results);
+        
+        // Fetch XPath select values
+        
+        $this->processSelect($dom, &$results);
+        
+        return 1;        
     }
 
 
+    protected function processNode($node, $xpath, &$results)
+    {
+        if ($node->nodeType !== XML_ELEMENT_NODE)
+            return;
+        
+        $ns_name = $node->localName;
+        
+        if (strlen($node->namespaceURI) > 0)
+        {
+            $ns_name = '<' . $node->namespaceURI . '>' . $ns_name;
+
+            if (! in_array($node->namespaceURI, $results[ 'all_namespaces' ]))
+                $results[ 'all_namespaces' ][ ] = $node->namespaceURI;
+        
+            if (strlen($node->prefix) > 0)
+                $this->namespace_prefixes[ $node->namespaceURI ] = $node->prefix;
+        }
+        
+        if (! in_array($ns_name, $results[ 'all_tags' ]))
+            $results[ 'all_tags' ][ ] = $ns_name;
+
+        $xpath .= $ns_name;
+
+        $this->addNodeInfo($xpath, $node->nodeValue, $results);
+        
+        if ($node->hasAttributes())
+        {
+            foreach ($node->attributes as $attribute)
+            {
+                if (! in_array($attribute->nodeName, $results[ 'all_attributes' ]))
+                    $results[ 'all_attributes' ][ ] = $attribute->nodeName;
+                    
+                $attrib_xpath = $xpath . '/@' . $attribute->nodeName;
+                
+                $this->addNodeInfo($attrib_xpath, $attribute->value, $results);
+            }
+        }
+        
+        // XXX to be implemented: detect mixed content, empty tags, text tags, ...
+        
+        if ($node->hasChildNodes())
+        {
+            foreach ($node->childNodes as $childnode)
+                $this->processNode($childnode, $xpath . '/', $results);
+        }
+    }    
+    
+
+    protected function addNodeInfo($xpath, $value, &$results)
+    {    
+        if (! isset($results[ 'all_xpaths' ][ $xpath ]))
+        {
+            $results[ 'all_xpaths' ][ $xpath ] = array
+            (
+                'm' => 0,
+                'l' => 0,
+                'b' => false
+            );
+        }
+
+        $results[ 'all_xpaths' ][ $xpath ][ 'm' ]++;
+    
+        $value = trim($value);
+        $l = strlen($value);
+
+        if ($l > $results[ 'all_xpaths' ][ $xpath ][ 'l' ])
+            $results[ 'all_xpaths' ][ $xpath ][ 'l' ] = $l;
+
+        // XXX we get multiline = yes for the XML root tag; does $node->nodeValue
+        // contain all child node values??
+        
+        if (is_int(strpos($value, "\n")) || is_int(strpos($value, "\r")))
+            $results[ 'all_xpaths' ][ $xpath ][ 'b' ] = true;
+    }
+    
+    
+    protected function processSelect(DOMDocument $dom, &$results)
+    {
+        if (! isset($this->options[ 'select' ]))
+            return;
+            
+        $xpath = new DOMXPath($dom);
+
+        // XXX note that cleanupNamespaces() has not been run yet (runs at the
+        // very end), so the user can only use prefixes explicitly defined in
+        // the document or specified using --xmlns in his XPath selects.
+        
+        foreach ($this->namespace_prefixes as $namespace_uri => $namespace_prefix)
+            $xpath->registerNameSpace($namespace_prefix, $namespace_uri);
+
+        foreach ($this->options[ 'select' ] as $select)
+        {
+            if (! isset($results[ 'select' ][ $select ]))
+                $results[ 'select' ][ $select ] = array();
+
+            foreach ($xpath->query($select) as $node)
+                $results[ 'select' ][ $select ][ ] = (string) $node->nodeValue;
+        }
+
+        foreach ($results[ 'select' ] as $select => $values)
+            $results[ 'select' ][ $select ] = array_unique($results[ 'select' ][ $select ]);
+    }
+    
+    
+    protected function cleanupNamespaces()
+    {
+        // If the XML does not declare any NsPrefix for a NsUri, auto-generate
+        // one
+        
+        $suffix = 1;
+        
+        foreach ($this->stats[ 'all_namespaces' ] as $namespace_uri)
+        {
+            if (isset($this->namespace_prefixes[ $namespace_uri ]))
+                continue;
+
+            while (true)
+            {
+                $namespace_prefix = 'ns' . $suffix;
+                
+                if (! in_array($namespace_prefix, $this->namespace_prefixes))
+                    break;
+                    
+                $suffix++;
+            }
+            
+            $this->namespace_prefixes[ $namespace_uri ] = $namespace_prefix;
+        }
+
+        // <NsUri>tag => NsPrefix:tag in "all_tags" and "all_xpaths"
+        
+        $replace = array();
+        
+        foreach ($this->namespace_prefixes as $namespace_uri => $namespace_prefix)
+            $replace[ '<' . $namespace_uri . '>' ] = $namespace_prefix . ':';
+
+        foreach ($this->stats[ 'all_tags' ] as $key => $tagname)
+            $this->stats[ 'all_tags' ][ $key ] = strtr($tagname, $replace);
+
+        $old_keys = array_keys($this->stats[ 'all_xpaths' ]);
+        
+        foreach ($old_keys as $old_key)
+        {
+            $new_key = strtr($old_key, $replace);
+            
+            if ($new_key === $old_key)
+                continue;
+                
+            $this->stats[ 'all_xpaths' ][ $new_key ] = $this->stats[ 'all_xpaths' ][ $old_key ];
+            
+            unset($this->stats[ 'all_xpaths' ][ $old_key ]);
+        }
+    }
+    
+    
     protected function summarize()
     {
         fwrite(STDOUT, sprintf
@@ -472,10 +531,27 @@ EOT;
             $this->stats[ 'xml_parse_error' ]
         ));
 
+        sort($this->stats[ 'all_namespaces' ]);
         sort($this->stats[ 'all_tags'       ]);
         sort($this->stats[ 'all_attributes' ]);
         ksort($this->stats[ 'all_xpaths'    ]);
 
+        fwrite(STDOUT, sprintf
+        (
+            "\n%d XML namespaces found:\n",
+            count($this->stats[ 'all_namespaces' ])
+        ));
+        
+        foreach ($this->stats[ 'all_namespaces' ] as $namespace_uri)
+        {
+            fwrite(STDOUT, sprintf
+            (
+                'xmlns:%s="%s"' . "\n",
+                $this->namespace_prefixes[ $namespace_uri ],
+                $namespace_uri
+            ));
+        }
+        
         fwrite(STDOUT, sprintf
         (
             "\n%d XML tags found:\n%s\n",
@@ -490,12 +566,12 @@ EOT;
             implode(', ', $this->stats[ 'all_attributes' ])
         ));
 
-        fwrite(STDOUT, "\n" . str_repeat('=', 80) . "\n");
+        fwrite(STDOUT, "\n" . str_repeat('=', 100) . "\n");
         
         fwrite
         (
             STDOUT,
-            str_pad('XPath', 56) .
+            str_pad('XPath', 76) .
             str_pad('Files', 6, ' ', STR_PAD_LEFT) .
             str_pad('Occur', 6, ' ', STR_PAD_LEFT) .
             str_pad('Length', 8, ' ', STR_PAD_LEFT) .
@@ -503,14 +579,14 @@ EOT;
             "\n"
         );
         
-        fwrite(STDOUT, str_repeat('=', 80) . "\n");
+        fwrite(STDOUT, str_repeat('=', 100) . "\n");
 
         foreach ($this->stats[ 'all_xpaths' ] as $xpath => $values)
         {
             fwrite
             (
                 STDOUT,
-        		str_pad($xpath, 56) .
+        		str_pad($xpath, 76) .
         		str_pad($values[ 'f' ], 6, ' ', STR_PAD_LEFT) .
         		str_pad($values[ 'm' ], 6, ' ', STR_PAD_LEFT) .
         		str_pad($values[ 'l' ], 8, ' ', STR_PAD_LEFT) .
@@ -519,23 +595,23 @@ EOT;
             );
         }
 
-        fwrite(STDOUT, str_repeat('=', 80) . "\n");
+        fwrite(STDOUT, str_repeat('=', 100) . "\n");
 
         foreach ($this->stats[ 'select' ] as $key => $values)
         {
-            fwrite(STDOUT, "\n" . str_repeat('=', 80) . "\n");
-            fwrite(STDOUT, str_pad('Values for ' . $key, 70) . str_pad('Files', 10, ' ', STR_PAD_LEFT) . "\n");
-            fwrite(STDOUT, str_repeat('=', 80) . "\n");
+            fwrite(STDOUT, "\n" . str_repeat('=', 100) . "\n");
+            fwrite(STDOUT, str_pad('Values for ' . $key, 90) . str_pad('Files', 10, ' ', STR_PAD_LEFT) . "\n");
+            fwrite(STDOUT, str_repeat('=', 100) . "\n");
 
             ksort($values);
 
             foreach ($values as $value => $count)
             {
-                // We're assuming ISO-8859-1 terminal output
-                fwrite(STDOUT, str_pad(utf8_decode($value), 70) . str_pad($count, 10, ' ', STR_PAD_LEFT) . "\n");
+                // We're assuming UTF-8 terminal output is okay...
+                fwrite(STDOUT, str_pad($value, 90) . str_pad($count, 10, ' ', STR_PAD_LEFT) . "\n");
             }
 
-            fwrite(STDOUT, str_repeat('=', 80) . "\n");
+            fwrite(STDOUT, str_repeat('=', 100) . "\n");
         }
     }
 }
